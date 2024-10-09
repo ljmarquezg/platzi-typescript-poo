5748148007d85b6a16534f63601553dcfdc1a8cb
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-10-09 15:10:44 +0200

    Function to Class

---
+++ src/app/services/product-memory.service.ts
@@ -0,0 +1,41 @@
+import { faker } from "@faker-js/faker";
+
+import { Product } from '../models/product.model';
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+
+
+export class ProductMemoryService {
+    private _products: Product[] = [];
+
+    create(data: CreateProductDto): Product {
+        const newProduct = {
+            ...data,
+            id: faker.number.int(),
+            category: {
+                id: data.categoryId,
+                name: faker.commerce.department(),
+                image: faker.image.url(),
+            }
+        }
+        return this.add(newProduct);
+    }
+
+    add(product: Product): Product {
+        this._products.push(product);
+        return product;
+    }
+
+    updateProduct(id: Product['id'], changes: UpdateProductDto ): Product {
+        const index = this._products.findIndex(item => item.id === id);
+        const prevData = this._products[index];
+        this._products[index] = {
+            ...prevData,
+            ...changes
+        }
+        return this._products[index];
+    }
+
+    findOne(id: Product['id']): Product | undefined {
+        return this._products.find(item => item.id === id);
+    }
+};
