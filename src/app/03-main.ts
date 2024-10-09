20356eec04e7648138e8a96a38703aec73e0fe89
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-10-09 15:48:45 +0200

    Product HTTP

--- src/app/03-main.ts
+++ src/app/03-main.ts
@@ -2,19 +2,31 @@
 import { ProductHttpService } from "./services/product-http.service";
 
 const productService = new ProductHttpService();
+(async () => {
+    try {
+        console.log('---'.repeat(10));
+        console.log('getAll');
+        const products = await productService.getAll();
+        console.log(products.length);
+        console.log(products.map(product => `${product.id} ${product.title}`)); 
+        
+        const productId = products[0].id;
+        
+        console.log('---'.repeat(10));
+        console.log('Update');
+        await productService.update(productId,{
+            title: 'Product 1 updated',
+            price: 900,
+            description: 'Description of product 1 updated',
+        });
 
-productService.create({
-    categoryId: 1,
-    title: 'Product 1',
-    price: 100,
-    description: 'Description of product 1',
-    images: []
-});
+        console.log('---'.repeat(10));
+        console.log('findOne');
+        const productUpdated = await productService.findOne(productId);
+        console.log(productUpdated); 
+    } catch (error) {
+        console.error(error);
+    }
+})();
 
-const products = productService.getAll();
-const productId = products[0].id;
-productService.update(productId, {
-    title: 'Product 1 updated',
-});
-const response = productService.findOne(productId);
-console.log(response);
+
